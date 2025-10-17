2645c09e795820d8dfead36e089010201b3e2a97
Divya <divyasingh@tracebloc.io>
2025-10-17 19:28:24 +0530

    updated models

--- model_zoo/tabular_classification/pytorch/LSTM.py
+++ model_zoo/tabular_classification/pytorch/LSTM.py
@@ -9,7 +9,7 @@ category = "tabular_classification"
 num_feature_points = 69
 
 class SimpleLSTM(nn.Module):
-    def __init__(self, input_size=69, hidden_size=128, num_layers=1):
+    def __init__(self, input_size=num_feature_points, hidden_size=128, num_layers=1):
         super(SimpleLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 64)
