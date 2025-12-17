0569b645e413096188c60c3c5ce3493138403a58
Divya <divyasingh@tracebloc.io>
2025-12-17 13:00:09 +0530

    updated models

---
+++ model_zoo/tabular_classification/sklearn/cboost.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from catboost import CatBoostClassifier
+
+framework = "sklearn"
+model_type = "tree"
+main_method = "MyModel"
+batch_size = 4
+output_classes = 5
+category = "tabular_classification"
+num_feature_points = 50
+
+def MyModel():
+    return CatBoostClassifier(n_estimators=100, random_state=42, verbose=0)
+
