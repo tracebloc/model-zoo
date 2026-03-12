fdb05ecc016f6601e1c3c6756aabd349bc36fc98
Divya <divyasingh@tracebloc.io>
2026-03-12 13:26:18 +0530

    updated num_feature_points

--- model_zoo/tabular_classification/pytorch/FCN.py
+++ model_zoo/tabular_classification/pytorch/FCN.py
@@ -4,9 +4,9 @@ framework = "pytorch"
 model_type = ""
 main_class = "SimpleFCN"
 batch_size = 4096
-output_classes = 4
+output_classes = 2
 category = "tabular_classification"
-num_feature_points = 202
+num_feature_points = 253
 
 class SimpleFCN(nn.Module):
     def __init__(self):
