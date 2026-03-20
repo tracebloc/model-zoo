d6e138050ff063ba9e5877e792cdfea84e1a5244
Divya <divyasingh@tracebloc.io>
2026-03-20 15:40:47 +0530

    tensorflow added to tabular classification

---
+++ model_zoo/tabular_classification/tensorflow/CNN.py
@@ -0,0 +1,24 @@
+from tensorflow.keras import layers, models
+
+framework = "tensorflow"
+main_method = "MyModel"
+model_type = ""
+batch_size = 4096
+output_classes = 3
+category = "tabular_classification"
+num_feature_points = 152
+
+
+def MyModel(input_size=num_feature_points, n_outputs=1):
+    model = models.Sequential(
+        [
+            layers.Input(shape=(input_size, 1)),
+            layers.Conv1D(16, 3, padding="same", activation="relu"),
+            layers.Conv1D(32, 3, padding="same", activation="relu"),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(n_outputs, activation="sigmoid"),
+        ]
+    )
+    return model
---
+++ model_zoo/tabular_classification/tensorflow/FCN.py
@@ -0,0 +1,21 @@
+from tensorflow.keras import layers, models
+
+framework = "tensorflow"
+main_method = "MyModel"
+model_type = ""
+batch_size = 4096
+output_classes = 3
+category = "tabular_classification"
+num_feature_points = 152
+
+
+def MyModel(input_shape=(num_feature_points,), n_outputs=1):
+    model = models.Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(n_outputs, activation="sigmoid"),
+        ]
+    )
+    return model
